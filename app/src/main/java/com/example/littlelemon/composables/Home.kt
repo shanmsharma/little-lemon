5250298ae10c1f169866502a7b05e32daa4eaa04
shanmsharma <shanmsharma@gmail.com>
2024-11-24 18:38:08 -0800

    updated color

--- app/src/main/java/com/example/littlelemon/composables/Home.kt
+++ app/src/main/java/com/example/littlelemon/composables/Home.kt
@@ -32,7 +32,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
